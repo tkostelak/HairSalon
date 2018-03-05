d8a4a85490dcdfa9fd25856ab2df0c90c406971d
tyler kostelak <tkostelak@gmail.com>
2018-03-04 19:43:59 -0800

    Form adds capacity for user to create specialties on the salon website.

---
+++ HairSalon/Controllers/SpecialtyController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System;
+
+namespace HairSalon.Controllers
+{
+    public class SpecialtyController : Controller
+    {
+      [HttpGet("/specialty/add")]
+      public ActionResult SpecialtyAddForm()
+      {
+        return View("SpecialtyForm");
+      }
+
+      [HttpGet("/specialty")]
+      public ActionResult Specialties()
+      {
+        List<Specialty> specialtyList = Specialty.GetAllSpecialties();
+        return View(specialtyList);
+      }
+
+      [HttpPost("/specialty")]
+      public ActionResult CreateSpecialty()
+      {
+        string specialtyName = Request.Form["specialtyName"];
+
+        Specialty newSpecialty = new Specialty( specialtyName );
+        newSpecialty.SaveSpecialty();
+        List<Specialty> specialtyList = Specialty.GetAllSpecialties();
+
+        return View("Specialties", specialtyList);
+      }
+    }
+  }
